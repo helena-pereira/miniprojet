59cad36e2053742851ddfae5e32a46c0d32bedcc
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2017-06-02 08:40:06 +0200

    commit initial

---
+++ src/main/java/ch/hevs/businessobject/Category.java
@@ -0,0 +1,32 @@
+package ch.hevs.businessobject;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Category")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long idCategory;
+	@Column(name="Category")
+	private String descCategory;
+	
+
+	// relation
+	@OneToMany(mappedBy="Book", cascade = CascadeType.ALL)
+	private Set<Book> books;
+
+	public Category(){
+		
+	}
+	
+	
+	public Category(Long idCategory, String descCategory){
+		this.idCategory = idCategory;
+		this.descCategory = descCategory;
+	}
+
+}
